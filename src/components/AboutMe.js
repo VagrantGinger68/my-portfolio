96cee8c01f1c369fd1abbe8897b65e3635b0eeac
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2024-01-18 20:38:21 -0500

    add about me information

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,12 @@ const AboutMe = () => {
   return (
     <div>
       <h2>About Me</h2>
-      <p>Your introduction goes here.</p>
+      <p>
+        I have always been passionate about technology and how it affects our lives. Web development allows me to create
+        applications that can help make people's lives easier. My specialty is front-end development and I have experience
+        with the full stack. My preferred stack is React, JavaScript, HTML, and CSS. I also have experience with Node,
+        Express, SQL, and Git.
+      </p>
     </div>
   );
 };
