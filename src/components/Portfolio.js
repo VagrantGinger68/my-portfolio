c1fe3987bf9e58a4eacd386b829ee0e66042f0d5
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2024-01-18 20:38:29 -0500

    fill in some projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,8 +5,13 @@ const Portfolio = () => {
   return (
     <div>
       <h2>Portfolio</h2>
-      <ProjectCard title="Project 1" description="Description of Project 1" githubLink="https://github.com/project1" />
-      <ProjectCard title="Project 2" description="Description of Project 2" githubLink="https://github.com/project2" />
+      <ProjectCard title="MovieMate" description="Explore a vast movie library and join movie-specific chat rooms to connect with fellow
+      film enthusiasts, sharing your thoughts, reviews, and recommendations in real time."
+        githubLink="https://github.com/VagrantGinger68/MovieMate" />
+
+      <ProjectCard title="Decision Maker" description="Decision Maker allows a user to create a poll and send it to their friends to help make
+decisions.
+" githubLink="https://github.com/VagrantGinger68/Decision-Maker_Midterm-Project" />
       {/* Add more projects as needed */}
     </div>
   );
